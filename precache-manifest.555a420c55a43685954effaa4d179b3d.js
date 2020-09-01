48812afab198a24f9aff12f24e62f5369c148900
Carms Ng <carmenhyng89@gmail.com>
2020-08-31 21:51:09 -0400

    Updates

---
+++ precache-manifest.555a420c55a43685954effaa4d179b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75b5687c33d8e695518c15b28c41a77",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "9fb816d19743b7c1c3ea",
+    "url": "/kanban-board/static/css/main.e994ed3a.chunk.css"
+  },
+  {
+    "revision": "5ff93b1038e5384de3b9",
+    "url": "/kanban-board/static/js/2.e01a1e7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-board/static/js/2.e01a1e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb816d19743b7c1c3ea",
+    "url": "/kanban-board/static/js/main.f8bb7fa4.chunk.js"
+  },
+  {
+    "revision": "ba2096fa3b770a19f0f1",
+    "url": "/kanban-board/static/js/runtime-main.8e469a6a.js"
+  }
+]);
